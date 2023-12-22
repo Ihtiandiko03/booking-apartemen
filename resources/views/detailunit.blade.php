1846de65701c021a0407e1b0caaa832015bb4d85
Ihtiandiko03 <wihtiandiko@gmail.com>
2023-12-22 16:02:12 +0700

    up

--- resources/views/detailunit.blade.php
+++ resources/views/detailunit.blade.php
@@ -12,30 +12,10 @@
 							<form class="col s12">
 								<div class="row">
 									<div class="col s12 avail-title">
-										<h4>Order</h4> </div>
+										<h1 class="text-center" style="color: white">{{$unit->nama_unit}}</h1>
+									</div>
 								</div>
-								<div class="row">
-									<div class="input-field col s12 m4 l2">
-										<select>
-											<option value="" disabled selected>Durasi</option>
-											<option value="Harian">Harian</option>
-											<option value="Mingguan">Mingguan</option>
-											<option value="Bulanan">Bulanan</option>
-											<option value="Tahunan">Tahunan</option>
-										</select>
-									</div>
-									
-									<div class="input-field col s12 m4 l2">
-										<input type="text" id="from" name="from">
-										<label for="from">Arrival Date</label>
-									</div>
-									<div class="input-field col s12 m4 l2">
-										<input type="text" id="to" name="to">
-										<label for="to">Departure Date</label>
-									</div>
-									<div class="input-field col s12 m4 l2">
-										<input type="submit" value="submit" class="form-btn"> </div>
-								</div>
+								
 							</form>
 						</div>
 					</div>
@@ -48,14 +28,14 @@
 				<div class="row">
 					<div class="col-md-8">
 						<div class="row">
-							<div class="hp-section">
+							{{-- <div class="hp-section">
 								<div class="hp-sub-tit">
 									<h4><span>{{$unit->nama_unit}}</span></h4>
 								</div>
 								<div class="hp-amini">
 									<p>{{$unit->deskripsi_unit}}</p>
 								</div>
-							</div>
+							</div> --}}
 							
 							<div class="hp-section">
 								<div class="hp-sub-tit">
@@ -163,6 +143,85 @@
 							
 						</div>
 					</div>
+					
+					<div class="col-md-4">
+						<div class="hp-call hp-right-com">
+							<div class="hp-call-in">
+								<h1>Price</h1>
+								<h3>Rp5.000.000</h3> 
+								<small class="mb-3">We are available 24/7 Monday to Sunday</small> <br><br>
+
+								<ul class="nav nav-tabs" role="tablist">
+									<li data-tab="Daily" class="active">
+										<a style="font-size: 6.5pt;" id="daily-tab" href="#daily" class="tab-control" aria-controls="daily" role="tab" data-toggle="tab" aria-expanded="true">Harian</a>
+									</li>
+									<li data-tab="Week">
+										<a style="font-size: 6.5pt;" id="week-tab" href="#week" class="tab-control" aria-controls="week" role="tab" data-toggle="tab" aria-expanded="false">Mingguan</a>
+									</li>
+									<li data-tab="Monthly">
+										<a style="font-size: 6.5pt;" id="monthly-tab" href="#monthly" class="tab-control" aria-controls="monthly" role="tab" data-toggle="tab" aria-expanded="false">Bulanan</a>
+									</li>
+									<li data-tab="Yearly">
+										<a style="font-size: 6.5pt;" id="yearly-tab" href="#yearly" class="tab-control" aria-controls="yearly" role="tab" data-toggle="tab">Tahunan</a>
+									</li>
+								</ul>
+								
+								<br>
+
+								<div class="row g-3">
+									<div class="col-xs-6">
+										<div>Check - In</div>
+											<input type="date" class="form-control" placeholder="Check-in" >
+										</div>
+									<div class="col-xs-6">
+										<div>Check - Out</div>
+											<input type="date" class="form-control"  placeholder="Check-out">
+									</div>
+
+									<div class="col-xs-12">
+										<br>
+										<div>Durasi</div>
+										<select id="months" class="form-select" name="months">
+											<option value="1" selected="selected">1 bulan</option>
+											<option value="2">2 bulan</option>
+											<option value="3">3 bulan</option>
+											<option value="4">4 bulan</option>
+											<option value="5">5 bulan</option>
+											<option value="6">6 bulan</option>
+											<option value="7">7 bulan</option>
+											<option value="8">8 bulan</option>
+											<option value="9">9 bulan</option>
+											<option value="10">10 bulan</option>
+											<option value="11">11 bulan</option>
+											<option value="12">12 bulan</option>
+										</select>
+									</div>
+
+									
+								</div>
+								<br>
+								<h3 style="color: #8a6e35">Total Rp5.000.000</h3> 
+								<button class="btn btn-danger">Booking</button>
+
+								<h5 style="text-align: left;">Info Penting : </h5>
+								<p style="text-align: justify;">Untuk pemesanan harian, deposit menggunakan bank transfer atau virtual account diestimasikan akan diterima kembali oleh pelanggan dalam waktu paling lambat 5 hari kerja setelah data bank terkonfirmasi. Pemberitahuan akan dikirimkan ke email Anda setelah berhasil (pastikan contact detail yang terdaftar aktif).</p>
+
+
+								
+
+								{{-- <select class="form-select" aria-label="Default select example">
+									<option selected>Open this select menu</option>
+									<option value="1">One</option>
+									<option value="2">Two</option>
+									<option value="3">Three</option>
+								  </select> --}}
+
+								
+
+							</div>
+						</div>
+					</div>
+
 					<div class="col-md-4">
 						<!--=========================================-->
 						<div class="hp-call hp-right-com">
@@ -171,6 +230,7 @@
 						</div>
 						
 					</div>
+					
 				</div>
 			</div>
 		</div>
